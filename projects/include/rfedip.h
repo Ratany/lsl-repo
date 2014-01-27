f32fda3d94b32238b78414ff68bf0fc49866d382
lee <lee@yun.yagibdah.de>
2014-01-27 04:21:43 +0100

    rfedip version 1.2

--- projects/include/rfedip.h
+++ projects/include/rfedip.h
@@ -19,7 +19,7 @@
 #ifndef _RFEDIP
 #define _RFEDIP
 
-#define RFEDIP_sVERSION                           "RFEDIP-1.1"  // protocol version
+#define RFEDIP_sVERSION                           "RFEDIP-1.2"  // protocol version
 #ifndef RFEDIP_sSUFFICIENT_VERSION
 #define RFEDIP_sSUFFICIENT_VERSION                "RFEDIP"  // default to any version as sufficient version
 #endif
@@ -27,10 +27,32 @@
 
 #define RFEDIP_CHANNEL                            -20131224  // default channel used for protocol messages
 
+
+// used to figure out whether a message is a RfE-dip message or not
+//
+#define RFEDIP_iMINMSGLEN                         6
+
+
+// protocol control messages
+//
+// control messages must always be sent on the default channel
+//
 #define RFEDIP_protIDENTIFY_QUERY                 "identify"  // identification query, must be responded to with RFEDIP_protIDENTIFY
 #define RFEDIP_protIDENTIFY                       "identity"  // answer to identification queries
 #define RFEDIP_protEND                            "msg_end!"  // indicate end of data transfer; if a particular channel was opened, this channel can be closed
 #define RFEDIP_protOPEN                           "openchan"  // open a particular channel for further communication, mandatorily has a channel number as parameter
+
+// useful messages
+//
+// These messages are included for their usefulness.  They are not
+// control messages so that they do not need to be sent on the default
+// channel.
+//
+// an "OK" message that can be sent to indicate that data was
+// received, that a requested operation was performed, etc.
+//
+#define RFEDIP_protOK                             "rfedipOK"
+
 
 #define RFEDIP_idxSENDER                          0
 #define RFEDIP_idxSENDER_UNIQ                     1
@@ -107,10 +129,30 @@
 //
 #define RFEDIP_RESPOND(_rcpt, _rcptUniq, _sndr, _chan, ...)           llRegionSayTo(_rcpt, _chan, RFEDIP_ToRESPONSE(_sndr, _rcpt, _rcptUniq, __VA_ARGS__))
 
-
-// used to figure out whether a message is a RfE-dip message or not
+// send an OK message, _sndr is this device, _nchan is the channel to
+// send the message on
 //
-#define RFEDIP_iMINMSGLEN                         6
+// this is NOT a control message
+//
+#define RFEDIP_OK(_rcpt, _rcptUniq, _sndr, _nchan)                    llRegionSayTo(_rcpt, _nchan, RFEDIP_ToRESPONSE(_sndr, _rcpt, _rcptUniq, RFEDIP_protOK))
+
+
+// macros to check version number
+//
+// Version numbers are composed of a leading string "RFEDIP-",
+// followed by an integer representing the major version number and an
+// integer representing the minor version number.  The two integers
+// are seperated by a dot (".").
+//
+// This means that version "RFEDIP-1.115" is a higher version number
+// than "RFEDIP-1.5".
+//
+// These two macros evaluate to the major and minor version numbers
+// when provided with a protocol message that was converted to list
+// _l.
+//
+#define RFEDIP_iVersionMajor(_l)                                      llList2Integer(llParseString2List(llList2String(llParseString2List(RFEDIP_ToPROTVERSION(_l), ["-"], []), 1), ["."], []), 0)
+#define RFEDIP_iVersionMinor(_l)                                      llList2Integer(llParseString2List(llList2String(llParseString2List(RFEDIP_ToPROTVERSION(_l), ["-"], []), 1), ["."], []), 1)
 
 
 #endif  // _RFEDIP
