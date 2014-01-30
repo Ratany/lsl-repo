262e34a24eb5ca8d59022ff6ecec207c3d83c29e
lee <lee@yun.yagibdah.de>
2014-01-30 09:13:25 +0100

    update to undetermined.h:  brackets

--- projects/include/undetermined.h
+++ projects/include/undetermined.h
@@ -5,12 +5,12 @@
 
 // sometimes stuff is undetermined
 //
-#define fUNDETERMINED              -1.0
-#define iUNDETERMINED              -1
+#define fUNDETERMINED              (-1.0)
+#define iUNDETERMINED              (-1)
 #define vUNDETERMINED              (<fUNDETERMINED, fUNDETERMINED, fUNDETERMINED>)
 
 #define fIsUndetermined(_f)        (fUNDETERMINED == _f)
-#define iIsUndetermined(_i)        (!~_i)
+#define iIsUndetermined(_i)        (!~(_i))
 #define vIsUndetermined(_v)        (vUNDETERMINED == _v)
 
 
