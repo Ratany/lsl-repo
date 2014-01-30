4cb3d2dbf93d36b05bbe73e37ed3dfd2eb6bd8b8
lee <lee@yun.yagibdah.de>
2014-01-30 04:31:29 +0100

    another thing too useful for not to have it:
    	new file:   projects/include/undetermined.h

---
+++ projects/include/undetermined.h
@@ -0,0 +1,17 @@
+
+#ifndef _UNDETERMINED
+#define _UNDETERMINED
+
+
+// sometimes stuff is undetermined
+//
+#define fUNDETERMINED              -1.0
+#define iUNDETERMINED              -1
+#define vUNDETERMINED              (<fUNDETERMINED, fUNDETERMINED, fUNDETERMINED>)
+
+#define fIsUndetermined(_f)        (fUNDETERMINED == _f)
+#define iIsUndetermined(_i)        (!~_i)
+#define vIsUndetermined(_v)        (vUNDETERMINED == _v)
+
+
+#endif
